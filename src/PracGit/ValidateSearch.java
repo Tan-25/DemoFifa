1f1f51fcbcb73ab5b1eb23fd7c5da44c138d7818
Tan-25 <taniya2598@gmail.com>
2022-12-04 15:08:34 -0500

    My Practice Initial Commit

---
+++ src/PracGit/ValidateSearch.java
@@ -0,0 +1,38 @@
+package PracGit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ValidateSearch {
+
+	@Test
+	public void Search() {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+
+		driver.findElement(By.name("q")).sendKeys("fifa world cup 2022");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
+		WebElement firstLink = driver.findElement(By.xpath("//div[@class='ofy7ae'][1]"));
+		String actualText = firstLink.getText();
+		String expected = "FIFA World Cup Qatar 2022™";
+		Assert.assertTrue(actualText.equalsIgnoreCase(expected),
+		"Expected " +expected+ " but found" + actualText );
+
+		}
+		
+			
+		
+		
+	}
